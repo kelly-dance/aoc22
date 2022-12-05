9c7eee616ccf7ae7d2574ca85f898605e2e82697
mcbobby123 <kellybob3737@gmail.com>
2022-12-05 14:15:38 -0700

    day 5

--- src/04/04.kt
+++ src/04/04.kt
@@ -4,7 +4,7 @@ fun main(){
     val vals = lines.map { Regex("""(\d+)-(\d+),(\d+)-(\d+)""").find(it)!! }
         .map(::matchesToInts)
         .map { (a,b,c,d) -> Pair((a..b).toSet(),(c..d).toSet()) }
-    val p1 = vals.count { (a, b) -> a.containsAll(b) || b.containsAll(b) }
+    val p1 = vals.count { (a,b) -> a.containsAll(b) || b.containsAll(a) }
     val p2 = vals.count { (a,b) -> a.intersect(b).isNotEmpty() }
     println(p1)
     println(p2)
---
+++ src/05/05.kt
@@ -0,0 +1,37 @@
+import java.util.Stack
+
+fun solve(part: Int) {
+    val real = true
+    val text = readInput(real)
+    val n = if(real) 9 else 3
+    val m = if(real) 8 else 3
+    val lines = text.lines()
+    val stacks = mutableListOf<Stack<Char>>()
+    for(i in 0 until n)stacks.add(Stack())
+    for(line in 0 until m){
+        for(i in 0 until n){
+            val curline = lines[m-line-1]
+            val pos = i*4+1
+            if(pos >= curline.length) continue
+            val c = curline[pos]
+            if(c==' ')continue
+            stacks[i].add(c)
+        }
+    }
+    for(line in lines.drop(m+2)){
+        val (a,b,c) = matchesToInts(Regex("""move (\d+) from (\d+) to (\d+)""").find(line)!!)
+        val temp = Stack<Char>();
+        for(idk in 0 until a){
+            temp.add(stacks[b-1].pop())
+        }
+        if(part == 2) temp.reverse()
+        for(x in temp) stacks[c-1].add(x)
+    }
+    val p1 = stacks.joinToString("") { it.peek().toString() }
+    println(p1)
+}
+
+fun main(){
+    solve(1)
+    solve(2)
+}
